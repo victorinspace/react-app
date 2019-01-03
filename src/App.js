70d52b206eecb009e996bfab47aef47b33b0f108
Victor Velasquez <victorv24@gmail.com>
2019-01-03 12:16:02 -0800

    TodoList Component
    Create TodoList Component that contains the
    form elements for inputing to do items.
    
    * Add form element.
    * Nested input and button element
      inside of form element.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import TodoList from "./TodoList";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <TodoList />
       </div>
     );
   }
